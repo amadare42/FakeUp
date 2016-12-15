3a59d763ab9984c539b29a484d72e55ff7bc502d
Amadare <Amadare42@gmail.com>
2016-12-15 19:07:28 +0200

    added more to-do's to do

--- FakeUp/Config/IFakeUpConfig.cs
+++ FakeUp/Config/IFakeUpConfig.cs
@@ -26,6 +26,69 @@ namespace FakeUp.Config
         ICollectionWith<TFakeObject> FillElementsOf<TCollection>(Expression<Func<TFakeObject, TCollection>> memberPath)
             where TCollection : IEnumerable;
 
+        // TODO: implement dealing with classes without parameterless c-tor
+
+
+        // TODO: fill types assignable from
+        /*
+         * like FillAll (by type)
+         *   .FillAssignableFrom<TMember>()
+         *
+         * like Fill<TMember> (by abs. path)
+         *   .FillAssignableFrom<TMember>(fakeObject => fakeObject.Member)
+         *
+         * like Fill<TMember, TMetaMember> (by relative type)
+         *   .FillAssignableFrom<TMember, TMetaMember>(member => member.MetaValue)
+         *
+         * /
+
+        // TODO: fill by name matching
+        /*
+         * .Fill<string>().NamedLike("*FirstName").OfType<string>().With(() => Faker.Names.Generate());
+         * .FillNamedLike("*FirstName").ThatAssignableFrom<string>().With(() => Faker.Names.Generate());
+         * .FillNamedLike("*Dummy*Object").With(() => new Dummy());
+         *
+         */
+
+        // TODO: saved config should have dedicated class
+        /*
+         * It should contain name and it should be possible to get filling rules information (including config tree).
+         */
+
+        // TODO: [after saved config dedicated class] add ability to add name for generated fillers
+        /*
+         * var nameFiller = FakeUp.Config.Create<User>(c => c.NameConfig("NameFiller").Fill(u => u.Name).With("name"));
+         * nameFiller.Name == "NameFiller"
+         */
+
+        // TODO: [after saved config dedicated class] add ability to dump info for
+        /*
+         * var nameFiller = FakeUp.Config.Create<User>("NameFiller", c => c.Fill(u => u.Name).With("name"));
+         * nameFiller.Name == "NameFiller"
+         */
+
+        // TODO: use config to fill member
+        /*
+         *
+         * var smithNameFiller = FakeUp.Config.Create<User>(c => c.Fill(user => user.Name).With("Agent Smith"));
+         * var personNameFiller = FakeUp.Config.Create<User>(c => c.Fill(user => user.Name).With(() => Faker.Names.Create());
+         *
+         * ...
+         * .FillElementsOf(foo => foo.UserCollection).With(userCreator)
+         * ...
+         * FakeUp.CreateBy(userCreator)
+         * ...
+         *
+         */
+
+        // TODO: add ability to set random collection size
+        /* so following call should be possible:
+         *
+         * .WithRandomCollectionsSize()
+         * .WithRandomCollectionsSize(100)
+         * .WithRandomCollectionsSize(10, 100)
+         */
+
         IFakeUpConfig<TFakeObject> WithCollectionsSize(int defaultSize);
 
         IFakeUpConfig<TFakeObject> WithCollectionsSize<TCollection>(
